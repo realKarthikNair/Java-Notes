84516879ca26db8faca277fd421630cd298dd81b
realkarthiknair <knairothera@gmail.com>
2023-03-29 10:56:45 +0530

    code

--- Lab Work/B. March 22/CallByReference.java
+++ Lab Work/B. March 22/CallByReference.java
@@ -1,3 +1,25 @@
+class Test{
+    int a, b;
+    Test (int i, int j)
+    {
+        a = i; 
+        b = j;
+    }
+
+
+void math(Test ob)
+{
+    ob.a*=2;
+    ob.b/=2;
+}
+}
+
 public class CallByReference {
-    
+    public static void main(String[] args)
+    {
+        Test ob = new Test (15, 20);
+        System.out.println("Before call: "+ob.a+" "+ob.b);
+        ob.math(ob);
+        System.out.println("After call: "+ob.a+" "+ob.b);
+    }
 }
